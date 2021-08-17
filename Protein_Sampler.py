41b48a89bb174e8abead12f7441430d60e8ed826
rikta43 <79046251+rikta43@users.noreply.github.com>
2021-08-17 01:13:20 +0100

    Update Protein_Sampler.py
    sampling

--- Protein_Sampler.py
+++ Protein_Sampler.py
@@ -1,5 +1,9 @@
 import MDAnalysis as mda
+import MDAnalysis
+from MDAnalysis.analysis import rms
 from MDAnalysis.coordinates.memory import MemoryReader
+from MDAnalysis.analysis import distances
+from MDAnalysis.tests.datafiles import PSF, DCD
 import numpy.linalg
 import numpy as np
 import numpy as lyzo
@@ -8,12 +12,19 @@ import re
 import csv
 import os
 import pdb
+import math
 from Bio import SeqIO
 #from pudb import set_trace
 from Bio.PDB import *
 from glob import glob
 import configparser
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import pdb;
+import random
+from pandas.plotting import scatter_matrix
+# from bhatta_dist import bhatta_dist
+
 # This class designed to read trajectory file , topology file and calculate distace measure for particular ATOM (CA).Code is aim # to adapt user atom selection
 class Protein_Data:
     
@@ -63,7 +74,7 @@ class Protein_Data:
         cAlphaSeq = np.array(scaled_df)
         # below lines of code used to sample topology data
         cAlphaSample = scaled_df.sample(frac=0.5, replace=True, random_state=1) 
-        print(cAlphaSample) 
+        #print(cAlphaSample) 
 
     def _distance_measure(self,u):
         # can access via atom name
@@ -72,15 +83,89 @@ class Protein_Data:
         cterm = u.select_atoms('name OC2')[-1]
 
         bb = u.select_atoms('name CA')  # a selection (AtomGroup)
-
-        for ts in u.trajectory:     # iterate through all frames
+        # printing coordinates , based on that information how to pick the frame ?
+        print("----------------------")
+        print("COORDINATES INFORMATION")
+        print("----------------------")
+        print(bb.positions)
+        rgyr1 = []
+        time1 = []
+        d1 = []
+        r1 = []
+        print("----------------------")
+        print("TRAJECTORY INFORMATION")
+        print("----------------------")
+        for ts in u.trajectory[:6]:     # iterate through all frames
             r = cterm.position - nterm.position # end-to-end vector from atom positions
             d = numpy.linalg.norm(r)  # end-to-end distance
+            time = u.trajectory.time
             rgyr = bb.radius_of_gyration()  # method of AtomGroup
-            print("frame = {0}: d = {1} A, Rgyr = {2} A".format(ts.frame, d, rgyr))
-
-            print("end-to-end vector::",r)
-            print("end-to-end distance::",d)
+            coordinates = np.random.rand(len(u.atoms), 3)
+            print("frame = {0}: d = {1} A,r = {2}, Rgyr = {3} A, time = {4}".format(ts.frame, d, r, rgyr, time))
+            time1.append(u.trajectory.time)
+            rgyr1.append(bb.radius_of_gyration()) 
+            d1.append(numpy.linalg.norm(r))
+            r1.append(cterm.position - nterm.position) 
+        # saving each attibutes in dataframe  
+        rgyr_df = pd.DataFrame(rgyr1, columns=['Radius of gyration (A)'], index=time1)
+        d_df = pd.DataFrame(d1, columns=['Distance'], index=time1)
+        r_df = pd.DataFrame(r1, columns=['Vector1','Vector2','Vector3'], index=time1)
+        rgyr_df.index.name = 'Time (ps)'
+        #  concat all dataframe into one to create single input file        
+        frames = [d_df,rgyr_df,r_df]
+        result = pd.concat(frames, join='inner', axis=1)
+        h = result.to_numpy();
+        print("----------------------")
+        print("DISTANCE MEASURE")
+        print("----------------------")
+        print(h)
+        # save dataframe as CSV file
+        #result.to_csv('file1.csv')
+        self._statistical_analysis(result)
+       
+        # calling method to perfrom sampling 
+        self._trajectroy_sampling(result)
+        # self._bhatt_distance(h)
+        # Ploting graph for radius of gyration against time 
+        # rgyr_df.plot(title='Radius of gyration')
+        # plt.savefig("rgyrgraph.jpg")
+            
+    def _statistical_analysis(self,result):
+        print("----------------------")
+        print("STATISTICAL ANALYSIS")
+        print("----------------------")
+        print(result.describe())
+        # histogram for full trajectory
+        result.hist()
+        plt.savefig("FullTraj_Hist.jpg")
+        # commented due to dynamic image path giving error 
+        #plt.savefig(path["ImagePath"]+"FullTraj_Hist.jpg")
+        # scatter matrix for full trajectory
+        scatter_matrix(result, alpha=0.2, figsize=(6, 6), diagonal='kde')
+        plt.savefig("FullTraj_Scatter.jpg")
+        # commented due to dynamic image path giving error 
+        # plt.savefig(path["ImagePath"]+"FullTraj_Scatter.jpg")
+    
+    def _trajectroy_sampling(self,result):
+        print("----------------------")
+        print("SAMPLING")
+        print("----------------------")
+        # random sampling 
+        # axis=1  randomly sample columns and axis=0 for rows
+        # seed for the random number generator can be specified using randon_state , same rows & columns retuns each time
+        subset = result.sample(n=5,random_state=0)
+        print("random sampling",subset)
+        # histogram for subset 
+        subset.hist()
+        # commented due to dynamic image path giving error 
+        # plt.savefig(path["ImagePath"]+"Sample_Hist.jpg")
+        plt.savefig("Sample_Hist.jpg")
+        # scatter matrix for subset
+        scatter_matrix(subset, alpha=0.2, figsize=(6, 6), diagonal='kde')
+        # commented due to dynamic image path giving error 
+        plt.savefig("Sample_Scatter.jpg")
+        #plt.savefig(path["ImagePath"]+"Sample_Scatter.jpg")
+        
     def _select_atoms(self):
         print("4 select atoms")
         
@@ -93,6 +178,7 @@ def main():
         path = config['PROTEINFILE']
         trajectory_filename = path["BasePath"]+path["trajectory"]
         topology_filename = path["BasePath"]+path["topology"]
+        
         prodata = Protein_Data(trajectory_filename,topology_filename)
         traj = prodata.trajectory_data
         # selecting atom called "C Aplha" from trajectory file
@@ -100,7 +186,11 @@ def main():
         len(traj.atoms)
         # printing coordinated for C Alpha
         coordinates = np.random.rand(len(traj.atoms), 3)
-        print(coordinates)
+        # convert dataframe to numpy array
+        
+        #print(coordinates)
+       
         print(os.getcwd())
+        
 if __name__=='__main__':
     main()
