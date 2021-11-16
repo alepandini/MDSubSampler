07330d5145a8654686d0df41c10fcfe855df3179
Namir Oues <namir.oues@gmail.com>
2021-11-16 22:44:38 +0000

    Extend tests

--- RMSD_test_1.py
+++ RMSD_test_1.py
@@ -7,7 +7,19 @@ prot_data = MDSS_Protein_Sampler.ProteinData(
     config_parameters=None,
 )
 
-# Create a ProteinProperty object
-prot_property = MDSS_Protein_Sampler.ProteinProperty(
-    prot_data,
-)
+prot_data.output_trj_summary()
+# prot_data.statistical_analysis()   Discussion with Ale about the arguments
+# prot_data.add_property()           Same and about what is this method about
+
+# Sampling the protein with the Random sampler
+frame_list = list(range(1000))
+prot_sample = MDSS_Protein_Sampler.RandomSampler(frame_list, seed_number=1999)
+sample = prot_sample.sample(100)
+
+# Create a RMSDProperty object
+
+prot_property = MDSS_Protein_Sampler.RMSDProperty(prot_data, frame_list)
+
+sample_property = MDSS_Protein_Sampler.RMSDProperty(sample, frame_list)
+
+distance = MDSS_Protein_Sampler.BhattaDistance(prot_property, sample_property)
