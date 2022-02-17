b09eacc280abd2ffca6d8d7b0f16c5d6c9612c0e
Namir Oues <namir.oues@gmail.com>
2022-02-17 11:40:48 +0000

    Make appropriate changes to the mdss_protein_sampler_test after the code was split into several files

--- tests/mdss_protein_sampler_test.py
+++ tests/mdss_protein_sampler_test.py
@@ -1,12 +1,12 @@
 import random
-from mdss_protein_sampler import RandomSampler
-import mdss_protein_sampler
+import mdss_sampler
+import mdss_distance
 import pytest
 
 
 def test_random_sampler_sample_has_expected_length():
     frame_list = list(range(1000))
-    random_sampler = mdss_protein_sampler.RandomSampler(frame_list, seed_number=1999)
+    random_sampler = mdss_sampler.RandomSampler(frame_list, seed_number=1999)
     sample_list = random_sampler.sample(100)
 
     assert len(sample_list) == 100
@@ -14,7 +14,7 @@ def test_random_sampler_sample_has_expected_length():
 
 def test_random_sampler_sample_returns_subset():
     frame_list = list(range(1000))
-    random_sampler = mdss_protein_sampler.RandomSampler(frame_list, seed_number=1999)
+    random_sampler = mdss_sampler.RandomSampler(frame_list, seed_number=1999)
     sample_list = random_sampler.sample(100)
 
     assert all(x in frame_list for x in sample_list)
@@ -27,7 +27,7 @@ def test_calculate_distance():
 
     property_1 = FakeProperty()
     property_2 = FakeProperty()
-    distance = mdss_protein_sampler.Distance(property_1, property_2)
+    distance = mdss_distance.Distance(property_1, property_2)
 
     assert distance.distance == property_1.avg_value - property_2.avg_value
 
@@ -35,9 +35,9 @@ def test_calculate_distance():
 @pytest.mark.parametrize(
     "mocked_function_name, distance_subclass",
     [
-        ("dictances.bhattacharyya", mdss_protein_sampler.BhattaDistance),
-        ("dictances.kullback_leibler", mdss_protein_sampler.KLDiverDistance),
-        ("dictances.pearson", mdss_protein_sampler.PearsonDictDistance),
+        ("dictances.bhattacharyya", mdss_distance.BhattaDistance),
+        ("dictances.kullback_leibler", mdss_distance.KLDiverDistance),
+        ("dictances.pearson", mdss_distance.PearsonDictDistance),
     ],
 )
 def test_bhattacharyya_distance(mocker, mocked_function_name, distance_subclass):
