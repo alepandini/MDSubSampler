f6d19068c8ff67e3fc11bbcfbf67c138f22000e1
Namir Oues <namir.oues@gmail.com>
2022-03-10 13:11:11 +0000

    mdss_utilites: Create file with description

---
+++ mdss_utilities.py
@@ -0,0 +1,15 @@
+"""
+The utilities file consists of the following functions:
+
+- Function that checks the memory size of the machine that the user is using.
+- Function that checks the trajectory size that the user has used as an input.
+- Function that checks the number of threads that are available in the system 
+  (to impelent later).
+- Function that checks that the XTC file is not empty and is contains at least X 
+  number of frames.
+- Function that checks that the number of selected residues that are used as an
+  input matches the number of residues in the XTC file.
+- Function that checks that the PDB/XTC files are present in the given directory.
+- Funtion that checks if all given XTC files have the same number of atoms.
+
+"""
