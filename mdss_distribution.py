bc6a71c3654ae3e0a855fbff23242064b989d7cd
Namir Oues <namir.oues@gmail.com>
2022-04-05 11:37:47 +0100

    push code to work on office machine

--- mdss_distribution.py
+++ mdss_distribution.py
@@ -1,23 +1,28 @@
+import mdss_property
+
+
 class DistributionDistance:
     """
     A class used to compare the distributions between 2 property calculation over a trajectory
 
     Attributes
     ----------
-    property_full : 
-    
-    property_sample : 
-    
+    property_full :
+
+    property_sample :
+
     """
 
-    def __init__(self, property_full, property_sample):
+    def __init__(
+        self,
+        property_full_traj,
+        property_sam_traj,
+        # property_class,
+        # protein_data,
+        # distance_class,
+    ):
 
-        self.property_full = property_full
-        self.property_sample = property_sample
-
-
-
-    
-    
-    
-    
+        self.property_full_traj = property_full_traj
+        self.property_sam_traj = property_sam_traj
+        self.property_full_traj = mdss_property.ProteinProperty(self.property_full_traj)
+        self.property_sam_traj = mdss_property.ProteinProperty(self.property_sam_traj)
