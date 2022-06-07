96c806a17104069bc2cc48e2b62240a3b9290566
Namir Oues <namir.oues@gmail.com>
2022-06-07 12:23:34 +0100

    Add script_scenario1 shell script

---
+++ script_scenario1.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+#Drug design scenario: 
+
+#Select structures for drug design. 
+#Goal: Extract subset of frames with similar distribution of pocket size.
+
+#Sampling method: Random Sampling
+#Property Calculation: RMSD
+
+
+python mdss_parser.py \
+  --traj "data/user.xtc" \
+  --top "data/user.gro" \
+  --prefix "user" \
+  --output-folder "data/results" \
+  --property='RMSDProperty' \
+  --atom-selection='name CA' \
+  --sampler='RandomSampler' \
+  --seed-number=1999 \
+  --size=100 \
+  --distance='Distance'
