d8a5ea70c9ba8206eaf65ec06cce59861497e5e6
Namir Oues <namir.oues@gmail.com>
2022-07-04 20:49:41 +0100

    keep copy of original scenario 1

--- mdss_scenario_001.sh
+++ mdss_scenario_001.sh
@@ -35,3 +35,26 @@ python3.10 mdss_parser.py \
   --size=$SIZE \
   --distance=$DISTANCE
 
+
+# #!/usr/bin/env bash
+# """
+# Drug design scenario: 
+
+# Problem: Select protein structures from trajectory for drug design. 
+# Goal: Extract subset of frames with similar distribution of pocket size.
+
+# Sampling method: Random Sampling
+# Property Calculation: RMSD
+# """
+
+# python mdss_parser.py \
+#   --traj "data/user.xtc" \
+#   --top "data/user.gro" \
+#   --prefix "user" \
+#   --output-folder "data/results" \
+#   --property='RMSDProperty' \
+#   --atom-selection='name CA' \
+#   --sampler='RandomSampler' \
+#   --seed-number=1999 \
+#   --size=100 \
+#   --distance='Dissimilarity'
