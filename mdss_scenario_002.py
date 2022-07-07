a9cf16537365ba9be7d0f60a4dfc46ca3b13d47d
Namir Oues <namir.oues@gmail.com>
2022-07-07 15:06:51 +0100

    snenario_002: create main and check traj size

--- mdss_scenario_002.py
+++ mdss_scenario_002.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
-from mdss import main
+from mdss import main, sampling_workflow
+from mdss_utilities import check_trajectory_size as tr
 import sys
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    trj_filename = args[0]
-    top_filename = args[1]
-    out_prefix = args[2]
+out_dir = "testing"
+property = "RMSDProperty"
+selection = "name CA"
+sampler = "RandomSampler"
+size = "100"
+dissimilarity = "Dissimilarity"
 
-    out_dir = "testing"
-    property = "RMSDProperty"
-    selection = "name CA"
-    sampler = "RandomSampler"
-    size = "100"
-    dissimilarity = "Dissimilarity"
 
-    size_list = [100, 200, 300, 400, 500, 600]
+def percentage(part, whole):
+    percentage = 100 * float(part) / float(whole)
+    return percentage
+
+
+def main(trj_filename, top_filename, out_prefix):
+
+    traj_size = tr.check_trajectory_size(trj_filename, top_filename)
+    perc_90 = percentage(90, traj_size)
+    perc_80 = percentage(80, traj_size)
+    perc_70 = percentage(70, traj_size)
+    perc_60 = percentage(60, traj_size)
+    perc_50 = percentage(50, traj_size)
+    perc_40 = percentage(40, traj_size)
+
+    size_list = [perc_90, perc_80, perc_70, perc_60, perc_50, perc_40]
     for size in size_list:
 
-        dissimilarity_score = main(
+        dissimilarity_score = sampling_workflow(
             [
                 "--traj",
                 trj_filename,
@@ -42,9 +53,17 @@ if __name__ == "__main__":
                 dissimilarity,
             ]
         )
+        print("for {} percent sample of full trajectory:".format(size))
         print(dissimilarity_score)
         if dissimilarity_score < 0.001:
             break
 
+
+if __name__ == "__main__":
+    arg_list = sys.argv[1:]
+    main(arg_list)
+    trj_filename = args[0]
+    top_filename = args[1]
+    out_prefix = args[2]
 
 # python scenario.py data/user.xtc data/user.gro testing
