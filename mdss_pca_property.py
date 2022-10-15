053edfb84d00aa76425abf90b87a29fe41255f49
Alessandro Pandini <alessandro.pandini@gmail.com>
2022-10-15 16:04:33 +0100

    add function to choose single PC projection (default to first)

--- mdss_pca_property.py
+++ mdss_pca_property.py
@@ -17,11 +17,25 @@ class TrjPCA(ProteinProperty):
 
     display_name = "TrjPCA"
 
+    def __init__(self, protein_data, atom_selection='name CA'):
+        super().__init__(protein_data, atom_selection)
+        self.pca_moedl = None
+        self.n_pcs = None
+        self.ed_n_pcs = None
+        self.property_matrix = None
+
     def _run_pca(self):
         self.pca_model = pca.PCA(self.protein_data.trajectory_data, self.atom_selection)
         self.pca_model.run()
 
-    def calculate_property(self, var_threshold = 0.8, pc_filter = False):
+    def select_pc(self, pc_index=1):
+        if not self.n_pcs:
+            print("Warning: values not available. Please calculate property values.")
+        else:
+            self.property_vector = self.property_matrix[:,(pc_index-1)]
+            self.pc_index = pc_index
+
+    def calculate_property(self, var_threshold=0.8, pc_filter=False):
         self._run_pca()
         self.n_pcs = self.pca_model.n_components
         self.ed_n_pcs = np.where(self.pca_model.results.cumulated_variance > var_threshold)[0][0]
@@ -30,5 +44,6 @@ class TrjPCA(ProteinProperty):
             selected_pcs = self.ed_n_pcs
         else:
             selected_pcs = self.n_pcs
-        self.property_vector = self.pca_model.transform(atomgroup, n_components=selected_pcs)
+        self.property_matrix = self.pca_model.transform(atomgroup, n_components=selected_pcs)
+        self.select_pc()
 
