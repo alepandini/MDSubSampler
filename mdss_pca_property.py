3b4c952a57c231d9ad6cdb5078f860d49a52dcfe
Alessandro Pandini <alessandro.pandini@gmail.com>
2022-10-15 16:49:30 +0100

    add super class for PCs (loadings) and make score class a subclass

--- mdss_pca_property.py
+++ mdss_pca_property.py
@@ -3,19 +3,9 @@ import MDAnalysis.analysis.pca as pca
 import numpy as np
 
 
-class TrjPCAProj(ProteinProperty):
-    """
-    A Subclass of ProteinProperty class used to perform PCA on the protein
-    trajectory and calculates a pca_space vector with number of columns equal
-    the number of PCs and the number of rows equal the number of frames in the
-    trajectory.
+class TrjPCA(ProteinProperty):
 
-    Returns
-    ----------
-    pca_vector: A vector that will be used to sample the trajectory of the protein
-    """
-
-    display_name = "TrjPCAProj"
+    display_name = "TrjPCA"
 
     def __init__(self, protein_data, atom_selection='name CA'):
         super().__init__(protein_data, atom_selection)
@@ -37,6 +27,32 @@ class TrjPCAProj(ProteinProperty):
             else:
                 self.property_vector = self.property_matrix[:,(pc_index-1)]
                 self.pc_index = pc_index
+
+    def calculate_property(self, var_threshold=0.8, pc_filter=False):
+        self._run_pca()
+        self.n_pcs = self.pca_model.n_components
+        self.ed_n_pcs = np.where(self.pca_model.results.cumulated_variance > var_threshold)[0][0]
+        if pc_filter:
+            selected_pcs = self.ed_n_pcs
+        else:
+            selected_pcs = self.n_pcs
+        self.property_matrix = self.pca_model.results.p_components[:,:selected_pcs]
+        self.select_pc()
+
+
+class TrjPCAProj(TrjPCA):
+    """
+    A Subclass of ProteinProperty class used to perform PCA on the protein
+    trajectory and calculates a pca_space vector with number of columns equal
+    the number of PCs and the number of rows equal the number of frames in the
+    trajectory.
+
+    Returns
+    ----------
+    pca_vector: A vector that will be used to sample the trajectory of the protein
+    """
+
+    display_name = "TrjPCAProj"
 
     def calculate_property(self, var_threshold=0.8, pc_filter=False):
         self._run_pca()
