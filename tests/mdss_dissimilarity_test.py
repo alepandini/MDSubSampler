6e5459ba98bb2fbb0b3233f791ec663d00df45c2
Namir Oues <namir.oues@gmail.com>
2022-10-22 20:03:54 +0100

    Complete testing for dissimilarity class

--- tests/mdss_dissimilarity_test.py
+++ tests/mdss_dissimilarity_test.py
@@ -1,7 +1,6 @@
 from audioop import rms
 import mdss_dissimilarity
 import mdss_sampler
-import random
 import pytest
 
 
@@ -16,61 +15,22 @@ def test_calculate_dissimilarity(rmsd_property):
 
 
 @pytest.mark.parametrize(
-    "dissimilarity_subclass",
+    "dissimilarity_subclass, mocked_function",
     [
-        (mdss_dissimilarity.Bhattacharya),
-        (mdss_dissimilarity.KullbackLeibler),
-        (mdss_dissimilarity.Pearson),
+        (mdss_dissimilarity.Bhattacharya, "dictances.bhattacharyya"),
+        (mdss_dissimilarity.Pearson, "dictances.pearson"),
     ],
 )
-def test_dissimilarity_measure(dissimilarity_subclass, rmsd_property, spy_method):
+def test_dissimilarity_measure(
+    dissimilarity_subclass, mocked_function, mocker, rmsd_property
+):
     sampler = mdss_sampler.RandomSampler(rmsd_property)
     sampled_property = sampler.sample(100)
     dissimilarity = dissimilarity_subclass(sampled_property, rmsd_property)
-    spy = spy_method(dissimilarity, "calculate_dissimilarity")
+    function = mocker.patch(mocked_function)
+    function.return_value = 0
     dissimilarity.calculate_dissimilarity()
-    # spy.assert_called_once_with(
-    #     sampled_property.property_vector,
-    #     rmsd_property.property_vector,
-    # )
-    spy.assert_called_once()
-
-
-# def test_calculate_dissimilarity():
-#     class FakeProperty:
-#         def __init__(self):
-#             self.avg_value = random.random()
-
-#     target_property = FakeProperty()
-#     ref_property = FakeProperty()
-#     dissimilarity = mdss_dissimilarity.Dissimilarity(target_property, ref_property)
-
-#     assert (
-#         dissimilarity.calculate_dissimilarity
-#         == target_property.avg_value - ref_property.avg_value
-#     )
-
-
-# @pytest.mark.parametrize(
-#     "mocked_function_name, distance_subclass",
-#     [
-#         ("dictances.bhattacharyya", mdss_dissimilarity.Bhattacharya),
-#         ("dictances.kullback_leibler", mdss_dissimilarity.KullbackLeibler),
-#         ("dictances.pearson", mdss_dissimilarity.Pearson),
-#     ],
-# )
-# def test_bhattacharyya_distance(mocker, mocked_function_name, distance_subclass):
-#     class FakeProperty:
-#         def __init__(self):
-#             self.property_distribution_dict = random.random()
-
-#     mocked_function = mocker.patch(mocked_function_name)
-
-#     target_property = FakeProperty()
-#     ref_property = FakeProperty()
-#     distance_subclass(target_property, ref_property)
-
-#     mocked_function.assert_called_once_with(
-#         target_property.property_distribution_dict,
-#         ref_property.property_distribution_dict,
-#     )
+    function.assert_called_once_with(
+        dissimilarity.target_property.property_distribution_dict,
+        dissimilarity.ref_property.property_distribution_dict,
+    )
