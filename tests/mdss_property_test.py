d234071d503f6b0943179ba816443415ac711585
Namir Oues <namir.oues@gmail.com>
2022-10-22 20:34:11 +0100

    Complete tests for property class

--- tests/mdss_property_test.py
+++ tests/mdss_property_test.py
@@ -1,13 +1,8 @@
+from os import rmdir
 import tempfile
 import os.path
-
-"""       
-Test discretize_vector()
-golden tests - maybe later
-
-Tests methods of SampledProperty later
-
-"""
+import mdss_property
+import mdss_sampler
 
 
 def test_write_property_vector_method(rmsd_property):
@@ -22,3 +17,28 @@ def test_write_discretized_property_vector_method(rmsd_property):
         outfilepath = os.path.join(tmpdirname, "test.txt")
         rmsd_property.write_discretized_property_vector(outfilepath)
         assert os.path.exists(outfilepath)
+
+
+def test_get_samples_average(rmsd_property):
+    p_sampler = mdss_sampler.ProteinSampler(rmsd_property)
+    sampled_property = mdss_property.SampledProperty(
+        rmsd_property,
+        p_sampler.property_vector,
+        p_sampler.frame_indices,
+        p_sampler.samples_indices,
+    )
+    average_dict = sampled_property.get_samples_averages()
+    assert len(average_dict) == len(set(p_sampler.samples_indices))
+
+
+def test_get_samples_averages_method_called_in_get_averages(rmsd_property, spy_method):
+    p_sampler = mdss_sampler.ProteinSampler(rmsd_property)
+    sampled_property = mdss_property.SampledProperty(
+        rmsd_property,
+        p_sampler.property_vector,
+        p_sampler.frame_indices,
+        p_sampler.samples_indices,
+    )
+    spy = spy_method(sampled_property, "get_samples_averages")
+    sampled_property.get_average()
+    spy.assert_called_once()
