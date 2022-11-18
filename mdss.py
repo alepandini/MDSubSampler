e3003663d965454e6ab31c764c7d0ce00d523b56
Namir Oues <namir.oues@gmail.com>
2022-11-18 10:39:06 +0000

    Add comments to mdss.py

--- mdss.py
+++ mdss.py
@@ -5,15 +5,26 @@ import sys
 
 
 def sampling_workflow(arg_list):
+    """
+    Implements sampling of data and measures dissimilarity between distributions
+
+    Attributes
+    -----------
+    arg_list: List of arguments that were inputed by the user in the parser
+    """
     args = p.parse_args(arg_list)
     print(args)
 
     if not os.path.exists(args.output_folder):
         os.makedirs(args.output_folder)
 
+    # Create property class object with user's input property
     property_class = p.PROPERTY_CLASS_MAPPING[args.property]
+
+    # Check if user input includes an xvg file with precalculated property
     if args.xvg_file is not None:
         property = property_class.from_xvg(args.xvg_file)
+    # Otherwise read protein trajectory from input files and calculate property
     else:
         p_data = mdss_protein_data.ProteinData(
             args.trajectory_file, args.topology_file, config_parameters=None
@@ -21,6 +32,8 @@ def sampling_workflow(arg_list):
         property = property_class(p_data, args.atom_selection)
         property.calculate_property()
 
+    # Create sampler class object with user's input sampler
+    # Depending on the sampler ensure necessary arguments were provided
     sampler_class = p.SAMPLER_CLASS_MAPPING[args.sampler]
     if args.sampler == "RandomSampler":
         sampler = sampler_class(property, args.seed_number)
@@ -33,28 +46,34 @@ def sampling_workflow(arg_list):
     elif args.sampler == "BootstrappingSampler":
         sampler = sampler_class(property, args.number_of_iterations)
 
+    # Get the sample of the vector with calculated property with user's input size
     property_sample = sampler.sample(args.size)
+
+    # Create dissimilarity class object with user's input dissimilarity
     dissimilarity_class = p.DISSIMILARITY_CLASS_MAPPING[args.dissimilarity]
     dissimilarity_object = dissimilarity_class(property, property_sample)
+
+    # Calculate dissimilarity between full and sample trajectory
     dissimilarity_score = dissimilarity_object.calculate_dissimilarity()
 
-    print("Dissimilarity: {}".format(dissimilarity_score))
-
+    # Create file with calculated property for full trajectory
     filename = "{}_{}_{}.dat".format(
         args.file_prefix, property_class.display_name, dissimilarity_class.display_name
     )
     filepath = os.path.join(args.output_folder, filename)
-
     property.write_property_vector(filepath)
 
+    # Create file with calculated property for sample trajectory
     filename_sample = "{}_{}_sample_{}.dat".format(
         args.file_prefix, property_class.display_name, dissimilarity_class.display_name
     )
     filepath_sample = os.path.join(args.output_folder, filename_sample)
-
     property_sample.write_property_vector(filepath_sample)
 
+    # Crete file with data report that includes important statistics about trajectory
     p_data.property_data_report()
+
+    print("Dissimilarity: {}".format(dissimilarity_score))
 
     return dissimilarity_score
 
