ef91d0752508d640f0f477045f71a2deb5315852
Namir Oues <namir.oues@gmail.com>
2022-11-18 12:05:32 +0000

    Create test for parser

---
+++ tests/mdss_parser_test.py
@@ -0,0 +1,147 @@
+import mdss_parser as p
+import pytest
+
+
+def test_parser_with_all_arguments():
+    OUT_DIR = "testing"
+    PROPERTY = "RMSDProperty"
+    SELECTION = "name CA"
+    SAMPLER = "RandomSampler"
+    SIZE = "100"
+    DISSIMILARITY = "Dissimilarity"
+    TRJ_FILENAME = "input.gro"
+    TOP_FILENAME = "input.xtc"
+    OUT_PREFIX = "UNITTEST"
+
+    args_list = [
+        "--traj",
+        TRJ_FILENAME,
+        "--top",
+        TOP_FILENAME,
+        "--prefix",
+        OUT_PREFIX,
+        "--output-folder",
+        OUT_DIR,
+        "--property",
+        PROPERTY,
+        "--atom-selection",
+        SELECTION,
+        "--sampler",
+        SAMPLER,
+        "--seed-number",
+        "1999",
+        "--size",
+        str(SIZE),
+        "--dissimilarity",
+        DISSIMILARITY,
+    ]
+
+    args = p.parse_args(args_list)
+
+
+def test_parser_with_all_arguments():
+    OUT_DIR = "testing"
+    PROPERTY = "RMSDProperty"
+    SELECTION = "name CA"
+    SAMPLER = "RandomSampler"
+    SIZE = "100"
+    DISSIMILARITY = "Dissimilarity"
+    TRJ_FILENAME = "input.gro"
+    TOP_FILENAME = "input.xtc"
+    OUT_PREFIX = "UNITTEST"
+
+    args_list = [
+        "--traj",
+        TRJ_FILENAME,
+        "--top",
+        TOP_FILENAME,
+        "--prefix",
+        OUT_PREFIX,
+        "--output-folder",
+        OUT_DIR,
+        "--property",
+        PROPERTY,
+        "--atom-selection",
+        SELECTION,
+        "--sampler",
+        SAMPLER,
+        "--seed-number",
+        "1999",
+        "--size",
+        str(SIZE),
+        "--dissimilarity",
+        DISSIMILARITY,
+    ]
+
+    args = p.parse_args(args_list)
+
+
+def test_parser_with_missing_traj_file():
+    OUT_DIR = "testing"
+    PROPERTY = "RMSDProperty"
+    SELECTION = "name CA"
+    SAMPLER = "RandomSampler"
+    SIZE = "100"
+    DISSIMILARITY = "Dissimilarity"
+    TOP_FILENAME = "input.xtc"
+    OUT_PREFIX = "UNITTEST"
+
+    args_list = [
+        "--traj",
+        "--top",
+        TOP_FILENAME,
+        "--prefix",
+        OUT_PREFIX,
+        "--output-folder",
+        OUT_DIR,
+        "--property",
+        PROPERTY,
+        "--atom-selection",
+        SELECTION,
+        "--sampler",
+        SAMPLER,
+        "--seed-number",
+        "1999",
+        "--size",
+        str(SIZE),
+        "--dissimilarity",
+        DISSIMILARITY,
+    ]
+    with pytest.raises(SystemExit):
+        p.parse_args(args_list)
+
+
+def test_parser_with_missing_seeds_number():
+    OUT_DIR = "testing"
+    PROPERTY = "RMSDProperty"
+    SELECTION = "name CA"
+    SAMPLER = "RandomSampler"
+    SIZE = "100"
+    DISSIMILARITY = "Dissimilarity"
+    TRJ_FILENAME = "input.gro"
+    TOP_FILENAME = "input.xtc"
+    OUT_PREFIX = "UNITTEST"
+
+    args_list = [
+        "--traj",
+        TRJ_FILENAME,
+        "--top",
+        TOP_FILENAME,
+        "--prefix",
+        OUT_PREFIX,
+        "--output-folder",
+        OUT_DIR,
+        "--property",
+        PROPERTY,
+        "--atom-selection",
+        SELECTION,
+        "--sampler",
+        SAMPLER,
+        "--seed-number",
+        "--size",
+        str(SIZE),
+        "--dissimilarity",
+        DISSIMILARITY,
+    ]
+    with pytest.raises(SystemExit):
+        p.parse_args(args_list)
