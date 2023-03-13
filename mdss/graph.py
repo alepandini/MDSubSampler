265a757d5237bb78e91c376d592133986b375d59
Namir Oues <namir.oues@gmail.com>
2023-03-13 12:41:41 +0000

    Create overlapping distribution plots for property

--- mdss/graph.py
+++ mdss/graph.py
@@ -1,4 +1,8 @@
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MaxAbsScaler
+from matplotlib import style
 
 
 class PropertyPlot:
@@ -13,10 +17,12 @@ class PropertyPlot:
         path where output file with plot will be saved
     """
 
-    def __init__(self, protein_property, outfilepath):
-        self.protein_property = protein_property
-        self.property_vector = protein_property.property_vector
-        self.frame_indices = protein_property.frame_indices
+    def __init__(self, property, sampled_property, outfilepath):
+        self.property = property
+        self.property_vector = property.property_vector
+        self.sampled_property = sampled_property
+        self.sampled_property_vector = sampled_property.property_vector
+        self.frame_indices = property.frame_indices
         self.outfilepath = outfilepath
 
     def plot(self, prefix="property_vector", outfilepath=None):
@@ -32,4 +38,20 @@ class PropertyPlot:
         """
         if outfilepath is None:
             outfilepath = self.outfilepath
-        pass
+        # Convert lists with property to pandas dataframe
+        sample = self.sampled_property.property_vector
+        reference = self.property.property_vector
+        sample = pd.DataFrame(sample)
+        reference = pd.DataFrame(reference)
+
+        # Apply scaling to dataframes
+        max = MaxAbsScaler()
+        sample = max.fit_transform(sample)
+        reference = max.fit_transform(reference)
+
+        # Plot the density distribution of the data
+        plt.style.use("ggplot")
+        plt.hist(reference, bins=20, alpha=0.5, density=True, label="reference")
+        plt.hist(sample, bins=20, alpha=0.5, density=True, label="sample")
+        plt.legend(loc="upper right")
+        plt.savefig("plot.png")
