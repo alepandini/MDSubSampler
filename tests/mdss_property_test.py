eae0ac4182bb68cbb1cafdb78a7135523fccce90
Namir Oues <namir.oues@gmail.com>
2023-04-18 13:12:04 +0100

    Update tests in mdss_property_test.py

--- tests/mdss_property_test.py
+++ tests/mdss_property_test.py
@@ -3,6 +3,7 @@ import tempfile
 import os.path
 import mdss.property as p
 import mdss.sampler as s
+import mdss.parser as par
 
 
 def test_write_property_vector_method(rmsd_property):
@@ -27,25 +28,47 @@ def test_write_property_distribution_dict(rmsd_property):
 
 
 def test_get_samples_average(rmsd_property):
-    p_sampler = s.ProteinSampler(rmsd_property)
-    sampled_property = p.SampledProperty(
-        rmsd_property,
-        p_sampler.property_vector,
-        p_sampler.frame_indices,
-        p_sampler.samples_indices,
-    )
-    average_dict = sampled_property.get_samples_averages()
-    assert len(average_dict) == len(set(p_sampler.samples_indices))
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        file_prefix = "test"
+        sample_size = "100"
+        p_sampler = s.ProteinSampler(
+            rmsd_property,
+            rmsd_property.protein_data,
+            outfilepath,
+            file_prefix,
+        )
+        sampled_property = p.SampledProperty(
+            rmsd_property,
+            p_sampler.property_vector,
+            p_sampler.frame_indices,
+            p_sampler.samples_indices,
+            sample_size,
+            p_sampler.dissimilarity_measure,
+        )
+        average_dict = sampled_property.get_samples_averages()
+        assert len(average_dict) == len(set(p_sampler.samples_indices))
 
 
 def test_get_samples_averages_method_called_in_get_averages(rmsd_property, spy_method):
-    p_sampler = s.ProteinSampler(rmsd_property)
-    sampled_property = p.SampledProperty(
-        rmsd_property,
-        p_sampler.property_vector,
-        p_sampler.frame_indices,
-        p_sampler.samples_indices,
-    )
-    spy = spy_method(sampled_property, "get_samples_averages")
-    sampled_property.get_average()
-    spy.assert_called_once()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        file_prefix = "test"
+        sample_size = "100"
+        p_sampler = s.ProteinSampler(
+            rmsd_property,
+            rmsd_property.protein_data,
+            outfilepath,
+            file_prefix,
+        )
+        sampled_property = p.SampledProperty(
+            rmsd_property,
+            p_sampler.property_vector,
+            p_sampler.frame_indices,
+            p_sampler.samples_indices,
+            sample_size,
+            p_sampler.dissimilarity_measure,
+        )
+        spy = spy_method(sampled_property, "get_samples_averages")
+        sampled_property.get_average()
+        spy.assert_called_once()
