349f305f2934a7b806e40d37c94821852381f035
Namir Oues <namir.oues@gmail.com>
2023-04-18 13:16:11 +0100

    Update tests in mdss_dissimilarity_test.py

--- tests/mdss_dissimilarity_test.py
+++ tests/mdss_dissimilarity_test.py
@@ -1,17 +1,27 @@
 from audioop import rms
 import mdss.dissimilarity as d
 import mdss.sampler as s
+import tempfile
+import os.path
 import pytest
 
 
 def test_calculate_dissimilarity(rmsd_property):
-    sampler = s.RandomSampler(rmsd_property)
-    sampled_property = sampler.sample(100)
-    dissimilarity = d.Dissimilarity(sampled_property, rmsd_property)
-    assert (
-        dissimilarity.calculate_dissimilarity()
-        == sampled_property.avg_value - rmsd_property.avg_value
-    )
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        file_prefix = "test"
+        sampler = s.RandomSampler(
+            rmsd_property,
+            rmsd_property.protein_data,
+            outfilepath,
+            file_prefix,
+        )
+        sampled_property = sampler.sample(100)
+        dissimilarity = d.Dissimilarity(sampled_property, rmsd_property)
+        assert (
+            dissimilarity.calculate_dissimilarity()
+            == sampled_property.avg_value - rmsd_property.avg_value
+        )
 
 
 @pytest.mark.parametrize(
@@ -24,13 +34,21 @@ def test_calculate_dissimilarity(rmsd_property):
 def test_dissimilarity_measure(
     dissimilarity_subclass, mocked_function, mocker, rmsd_property
 ):
-    sampler = s.RandomSampler(rmsd_property)
-    sampled_property = sampler.sample(100)
-    dissimilarity = dissimilarity_subclass(sampled_property, rmsd_property)
-    function = mocker.patch(mocked_function)
-    function.return_value = 0
-    dissimilarity.calculate_dissimilarity()
-    function.assert_called_once_with(
-        dissimilarity.target_property.property_distribution_dict,
-        dissimilarity.ref_property.property_distribution_dict,
-    )
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        file_prefix = "test"
+        sampler = s.RandomSampler(
+            rmsd_property,
+            rmsd_property.protein_data,
+            outfilepath,
+            file_prefix,
+        )
+        sampled_property = sampler.sample(100)
+        dissimilarity = dissimilarity_subclass(sampled_property, rmsd_property)
+        function = mocker.patch(mocked_function)
+        function.return_value = 0
+        dissimilarity.calculate_dissimilarity()
+        function.assert_called_once_with(
+            dissimilarity.target_property.property_distribution_dict,
+            dissimilarity.ref_property.property_distribution_dict,
+        )
