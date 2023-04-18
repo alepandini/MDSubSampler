48c9a8d414c1efd3f1c0a5819ca285ac909d1e9e
Namir Oues <namir.oues@gmail.com>
2023-04-18 12:34:48 +0100

    Add test in property_test.py

--- tests/mdss_property_test.py
+++ tests/mdss_property_test.py
@@ -19,6 +19,13 @@ def test_write_discretized_property_vector_method(rmsd_property):
         assert os.path.exists(outfilepath)
 
 
+def test_write_property_distribution_dict(rmsd_property):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        rmsd_property.write_property_distribution_dict(outfilepath)
+        assert os.path.exists(outfilepath)
+
+
 def test_get_samples_average(rmsd_property):
     p_sampler = s.ProteinSampler(rmsd_property)
     sampled_property = p.SampledProperty(
