24035e8a17b3b2bea1fe1e25ae348b26af09c71c
Namir Oues <namir.oues@gmail.com>
2023-04-19 09:04:25 +0100

    Update tests/mdss_protein_data_test.py

--- tests/mdss_protein_data_test.py
+++ tests/mdss_protein_data_test.py
@@ -1,3 +1,7 @@
+import tempfile
+import os
+
+
 def test_frames_of_trajectory_has_expected_length(protein_data):
     frame_indices = protein_data._frames_of_trajectory()
     assert len(frame_indices) == len(protein_data.trajectory_data.trajectory)
@@ -26,13 +30,7 @@ def test_add_property_is_linked_to_property_data(protein_data, rmsd_property):
 
 
 def test_property_data_report(protein_data, rmsd_property):
-    data_report = protein_data.property_data_report()
-    assert len(data_report.keys()) == 1
-    key = list(data_report.keys())[0]
-    assert key.startswith(rmsd_property.display_name)
-    assert data_report[key] == {
-        "min": rmsd_property.min_value,
-        "max": rmsd_property.max_value,
-        "atom_selection": rmsd_property.atom_selection,
-        "name": rmsd_property.display_name,
-    }
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        outfilepath = os.path.join(tmpdirname, "test.txt")
+        protein_data.property_data_report(outfilepath)
+        assert os.path.exists(outfilepath)
